70e28b7c72ca93863128ac6b66571a6f32db19a2
Tuncer <Tuncer@172.31.14.249>
2018-11-28 13:31:06 +0100

    Edited ITurtleFX and added TurtleFX

--- TurtleFX/src/model/ITurtleFX.java
+++ TurtleFX/src/model/ITurtleFX.java
@@ -9,7 +9,7 @@ import javafx.geometry.Point2D;
  *  * facing 0 degrees<br>
  *  * pen up<br>
  *  * at the position x=0 and y=0<br><br>
- * The following defintion is used for defining where the turtle should face at which degree(mathematical one):<br>
+ * The following definition is used for defining where the turtle should face at which degree(mathematical one):<br>
  *  * 0 degrees = facing right<br>
  *  * 90 degrees = facing up<br>
  *  * 180 degrees = facing left<br>
@@ -42,18 +42,11 @@ public interface ITurtleFX {
 	abstract void forward(double length);
 	/**
 	 * Method to turn the turtle by the given angle.<br>
-	 * To use radians, look at the method "useRadians".
+	 * To use radians, look at the method "setRadians".
 	 * 
 	 * @param angle Angle the turtle should turn
 	 */
 	abstract void turn(double angle);
-	
-	/**
-	 * Method to decide whether degrees ir radians should be used.<br>
-	 * true = radians<br>
-	 * false = degrees (default)
-	 */
-	abstract void useRadians(boolean useRadians);
 	
 	/**
 	 * Method to set the current position of the turtle on the canvas.
@@ -64,23 +57,24 @@ public interface ITurtleFX {
 	/**
 	 * Method to set the current position of the turtle on the canvas.
 	 * 
-	 * @param pos Desired position of the turtle as x and y coordinates
+	 * @param posX Desired x position of the turtle as double
+	 * @param posY Desired y position of the turtle as double
 	 */
-	abstract void setPos(double x, double y);
+	abstract void setPos(double posX, double posY);
 	/**
 	 * Method to set the angle of the turtle.<br>
-	 * To use radians, look at the method "useRadians".
+	 * To use radians, look at the method "setRadians".
 	 * 
 	 * @param angle Angle the turtle should face
 	 */
 	abstract void setAngle(double angle);
-	
 	/**
-	 * Method to get whether the pen is up or not.
-	 * 
-	 * @return Whether the pen is up or not
+	 * Method to decide whether degrees ir radians should be used.<br>
+	 * true = radians<br>
+	 * false = degrees (default)
 	 */
-	abstract boolean isPenUp();
+	abstract void setRadians(boolean radians);
+	
 	/**
 	 * Method to get whether the pen is down or not.
 	 * 
@@ -95,7 +89,7 @@ public interface ITurtleFX {
 	abstract Point2D getPos();
 	/**
 	 * Method to get the angle the turtle is facing.<br>
-	 * To use radians, look at the method "useRadians".
+	 * To use radians, look at the method "setRadians".
 	 * 
 	 * @return Angle the turtle is facing
 	 */
--- TurtleFX/src/model/TurtleFX.java
+++ TurtleFX/src/model/TurtleFX.java
@@ -1,5 +1,108 @@
 package model;
 
-public class TurtleFX {
-	// TODO
+import javafx.geometry.Point2D;
+import javafx.scene.canvas.GraphicsContext;
+
+/**
+ * 
+ * @author Tuncer Catalkaya
+ * @version 1.0
+ */
+public class TurtleFX implements ITurtleFX {
+
+	public static final boolean DEFAULT_PENUP = true;
+	public static final boolean DEFAULT_PENDOWN = false;
+	public static final boolean DEFAULT_RADIANS = false;
+	
+	public static final double DEFAULT_ANGLE = 0d;
+	
+	public static final Point2D DEFAULT_POS = new Point2D(0d, 0d);
+	
+	
+	private boolean penDown = DEFAULT_PENDOWN;
+	private boolean radians = DEFAULT_RADIANS;
+	
+	private Point2D pos = DEFAULT_POS;
+	
+	private double angle = DEFAULT_ANGLE;
+	
+	private GraphicsContext gc;
+	
+	public TurtleFX(GraphicsContext gc) {
+		this.gc = gc;
+	}
+
+	@Override
+	public void penUp() {
+		if (penDown) {
+			penDown = !penDown;
+		}
+	}
+	
+	@Override
+	public void penDown() {
+		if (!penDown) {
+			penDown = !penDown;
+		}
+	}
+
+	@Override
+	public void forward(double length) {
+		double angleTmp = this.angle;
+		if (!radians) {
+			angleTmp = Math.toRadians(angleTmp);
+		}	
+		gc.strokeLine(pos.getX(), pos.getY(), length * Math.sin(angleTmp), length * Math.cos(angleTmp));
+	}
+
+	@Override
+	public void turn(double angle) {
+		if (radians) {
+			angle = Math.toRadians(angle);
+		}
+		this.angle += angle;
+	}
+
+	@Override
+	public void setPos(Point2D pos) {
+		this.pos = pos;
+	}
+
+	@Override
+	public void setPos(double posX, double posY) {
+		this.pos = new Point2D(posX, posY);
+	}
+
+	@Override
+	public void setAngle(double angle) {
+		if (radians) {
+			angle = Math.toRadians(angle);
+		}
+		this.angle = angle;
+	}
+
+	@Override
+	public void setRadians(boolean radians) {
+		this.radians = radians;
+	}
+
+	@Override
+	public boolean isPenDown() {
+		return penDown;
+	}
+
+	@Override
+	public Point2D getPos() {
+		return pos;
+	}
+
+	@Override
+	public double getAngle() {
+		return angle;
+	}
+
+	@Override
+	public boolean isRadians() {
+		return radians;
+	}
 }
